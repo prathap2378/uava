887925b76fb53df19ecf20bb2d270cc77d0ead43
bhanu.pk <bhanu.pk@wkssuresh.quadwave.lan>
2017-07-19 16:56:46 +0530

    removed econsys auto

--- econsys/Econsys_Auto/src/main/java/com/econsys/UIobjectrepositary/CP2CP3preparequote.java
+++
@@ -1,8 +0,0 @@
-package com.econsys.UIobjectrepositary;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class CP2CP3preparequote {
-
-}
--- econsys/Econsys_Auto/src/test/java/com/econsys/AllPages/RoleReassignment.java
+++
@@ -1,9 +0,0 @@
-package com.econsys.AllPages;
-
-import org.testng.annotations.Test;
-
-public class RoleReassignment {
-  @Test
-  public void f() {
-  }
-}
